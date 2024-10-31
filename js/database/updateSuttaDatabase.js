25001ba8661e862dc21c8f3d19c81b2fb4a24b89
gachi <59866185+Reptilioo@users.noreply.github.com>
2024-10-31 12:11:21 +0100

    Update updateSuttaDatabase.js

--- js/database/updateSuttaDatabase.js
+++ js/database/updateSuttaDatabase.js
@@ -3,29 +3,50 @@ import suttasCount from '../../python/generated/suttas-count.js'
 
 export default function updateSuttaDatabase()
 {
-  db.suttas.count().then((count) => {
+  db.suttas_en.count().then((count) => {
     const isEmpty = count === 0;
     const isDataMissing = suttasCount > count;
-  
+
     if (isEmpty || isDataMissing) {
-      fetch("../../python/generated/suttas-database-data.json").then(response => response.json()).then((suttas) => {
-        let suttasData = suttas;
-  
-        const data = Object.entries(suttasData).map(([key, value]) => ({
-          id: key,
-          value
-        }));
-  
-        db.suttas.bulkPut(data).then(() => {
-          const logMessage = isEmpty ? "[SUCCESS] Database setup complete" : "[SUCCESS] Database updated.";
-          console.log(logMessage);
-        }).catch((error) => {
-          console.error("[ERROR] Failed to insert data into database:", error);
+      function generateSortKey(id) {
+        return id.match(/\d+|\D+/g).map(chunk => isNaN(chunk) ? chunk : Number(chunk));
+      }
+      
+      fetch("../../python/generated/suttas-database-data.json")
+        .then((response) => response.json())
+        .then((suttas) => {
+          let suttasData = suttas;
+      
+          const dataEn = [];
+          const dataPl = [];
+      
+          Object.entries(suttasData).forEach(([key, value]) => {
+            const enEntry = {
+              id: key,
+              translation_en_anigha: value.translation_en_anigha || null,
+              heading: value.heading || null,
+              comment: value.comment || null,
+              sortKey: generateSortKey(key),  // Add sort key
+            };
+      
+            const plEntry = {
+              id: key,
+              root_pli_ms: value.root_pli_ms || null,
+              sortKey: generateSortKey(key),  // Add sort key
+            };
+      
+            dataEn.push(enEntry);
+            dataPl.push(plEntry);
+          });
+      
+          // Insert data in tables 
+          db.suttas_en.bulkPut(dataEn);
+          db.suttas_pl.bulkPut(dataPl);
+        })
+        .catch((error) => {
+          console.error("[ERROR] Failed to load suttas data:", error);
         });
-      }).catch((error) => {
-        console.error("[ERROR] Failed to load suttas data:", error);
-      });
-  
+
     } else {
       console.log("[INFO] Database is up to date. Skipped import.");
     }
