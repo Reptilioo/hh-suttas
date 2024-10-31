189a0471c2d00b174ad903ca2a760373368c45f1
gachi <59866185+Reptilioo@users.noreply.github.com>
2024-10-31 23:05:18 +0100

    Update advanced-search.js

--- advanced-search.js
+++ advanced-search.js
@@ -1,9 +1,15 @@
+//Problème d'affichage lorsqu'on cherche "cont"
+//Les passages étant sous forme de lien, on ne peut pas copier coller le texte qu'ils contiennent
+//Le texte extrait n'a pas l'air de commencer à partir du matching element
+//Faire en sorte que si le matching element apparaît plusieurs fois dans un même texte, tous les passages doivent être cités les uns à la suite des autres
+
 import db from "./js/dexie/dexie.js";
 import suttasCount from './python/generated/suttas-count.js'
 import { fetchAvailableSuttas } from "./js/utils/loadContent/fetchAvailableSuttas.js";
 
 const availableSuttasJson = await fetchAvailableSuttas();
 
+// Search function in the database
 async function searchSuttas(query, options) {
     const resultsDiv = document.querySelector('.results');
     resultsDiv.innerHTML = ''; // Clear previous results
@@ -15,19 +21,27 @@ async function searchSuttas(query, options) {
         // Convert to string to search in translation_en_anigha and comment
         const translationText = JSON.stringify(sutta.translation_en_anigha).toLowerCase();
         const commentText = JSON.stringify(sutta.comment).toLowerCase();
-    
-        if (translationText.includes(query.toLowerCase()) || commentText.includes(query.toLowerCase())) {
+		let inTrans = false;
+		let inComment = false;
+		
+        if (translationText.includes(query.toLowerCase())) 
+			inTrans = true;
+		
+		if (commentText.includes(query.toLowerCase()))
+			inComment = true;
+
+		if (inTrans || inComment){
             // Get the title from the JSON file
             const title = availableSuttasJson[sutta.id]?.title || "Unknown Title";
             const id = availableSuttasJson[sutta.id]?.id || sutta.id.toUpperCase();
     
-            // Extract the first 100 characters of the occurrence
+            // Test - Extract the first 100 characters of the occurrence
             let snippet = '';
-            if (translationText.includes(query.toLowerCase())) {
+            if (inTrans) 
                 snippet = translationText.substring(translationText.indexOf(query.toLowerCase()), 100);
-            } else if (commentText.includes(query.toLowerCase())) {
+            else 
                 snippet = commentText.substring(commentText.indexOf(query.toLowerCase()), 100);
-            }
+            
 
             // Create an HTML element for each result
             const resultDiv = document.createElement('div');
@@ -68,6 +82,5 @@ function getConfiguration() {
 document.querySelector('#search-button').addEventListener('click', () => {
     const query = document.querySelector('#search-input').value;
     const options = getConfiguration();
-    console.log(options);
     searchSuttas(query, options);
 });
