f43f9ff7c0194e490b9a05f00969af1975ae2d35
gachi <59866185+Reptilioo@users.noreply.github.com>
2024-11-05 22:02:34 +0100

    Update and rename checkForPaliUrlParameter to checkForPaliUrlParameter.js

--- js/utils/navigation/checkForPaliUrlParameter.js
+++ js/utils/navigation/checkForPaliUrlParameter.js
@@ -6,11 +6,11 @@ export function checkForPaliUrlParameter(){
 	const urlParams = new URLSearchParams(window.location.search);
 
 	if (urlParams.get('pali') === 'show' && localStorage.paliToggle === "hide") {
-    // If pali is shown but url parameter is set to "hide", hide pali
+    	// If pali is shown but url parameter is set to "hide", hide pali
 		suttaArea.classList.remove("hide-pali");
 		localStorage.paliToggle = "show";
 	} else if (urlParams.get('pali') === 'hide' && localStorage.paliToggle === "show") {
-    // If pali is hidden but url parameter is set to "show", show pali
+    	// If pali is hidden but url parameter is set to "show", show pali
 		suttaArea.classList.add("hide-pali");
 		localStorage.paliToggle = "hide";
 		document.body.classList.remove("side-by-side");
