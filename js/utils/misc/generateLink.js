d1a54ea8fba4d3841a5160355f968d3c5e0da4c6
gachi <59866185+Reptilioo@users.noreply.github.com>
2024-11-09 12:34:31 +0100

    Update generateLink.js

--- js/utils/misc/generateLink.js
+++ js/utils/misc/generateLink.js
@@ -11,15 +11,15 @@ function extractIdsFromUrl() {
 export function generateLink(idOrRange) {
   
   const suttaId = extractIdsFromUrl();
-    const baseUrl = window.location.origin + window.location.pathname;
-    let hash = "#";
-    if (typeof idOrRange === "string") {
-      hash += idOrRange;
-    } else if (Array.isArray(idOrRange) && idOrRange.length === 2) {
-      hash += `${idOrRange[0]}-${idOrRange[1]}`;
-    } else {
-      console.error("Invalid ID or Range format");
-      return "";
-    }
-    return `${baseUrl}?q=${suttaId}${hash}`;
+  const baseUrl = window.location.origin + window.location.pathname;
+  let hash = "#";
+  if (typeof idOrRange === "string") {
+    hash += idOrRange;
+  } else if (Array.isArray(idOrRange) && idOrRange.length === 2) {
+    hash += `${idOrRange[0]}_${idOrRange[1]}`;
+  } else {
+    console.error("Invalid ID or Range format");
+    return "";
   }
+  return `${baseUrl}?q=${suttaId}${hash}`;
+}
